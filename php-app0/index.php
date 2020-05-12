082a44cee0886b9b94e7e2a55f82e1b278f4df30
Haomin <haominhe@users.noreply.github.com>
2020-05-12 02:18:20 -0700

    update

--- php-app0/index.php
+++ php-app0/index.php
@@ -3,4 +3,19 @@
 // Go to http://localhost:4000/
 ?>
 
-<?php echo 'PHP works'; ?>
+<?php
+require 'functions.php';
+
+
+$task = [
+    'title' => 'Finish homework',
+    'due' => 'Today',
+    'assigned_to' => 'Mike',
+    'completed' => true
+];
+
+require 'index.view.php'
+
+
+
+?>
---
+++ php-app0/index.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+</head>
+
+<body>
+    <h1>Task For The Day</h1>
+    <ul>
+        <li><strong>Name: </strong><?= $task['title']; ?></li>
+        <li><strong>Due Date: </strong><?= $task['due']; ?></li>
+        <li><strong>Person Responsible: </strong><?= $task['assigned_to']; ?></li>
+        <li><strong>Status: </strong><?= $task['completed'] ? '&#9989; Complete' : 'Incomplete'; ?></li>
+    </ul>
+</body>
+
+</html>
