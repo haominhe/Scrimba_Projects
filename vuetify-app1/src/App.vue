6718acaf91cd3549a537f3ea726c5bfda4333e41
Haomin <haominhe@users.noreply.github.com>
2021-12-10 10:49:43 -0800

    updates

---
+++ vuetify-app1/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <TopNav />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import TopNav from "./components/TopNav";
+
+export default {
+  name: "App",
+
+  components: {
+    TopNav
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
